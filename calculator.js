1f5a544b61285cc39177cf2af0139003440b4fb0
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-10-03 23:34:15 -0700

    Add event listeners and logic for number buttons
    Number buttons update the display with corresponding number.
    Behavior depends on the display state

--- calculator.js
+++ calculator.js
@@ -20,3 +20,21 @@ function operate (a, b, operator) {
     if (operator === '*') return multiply(a, b);
     if (operator === '/') return divide(a, b);
 }
+
+const display = document.querySelector(".display");
+const numberBtns = document.querySelectorAll(".number");
+let displayValue;
+let displayState = 'freeze'
+
+numberBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (displayState === "freeze") {
+            display.textContent = btn.id;
+            displayState = "stack";
+        } else {
+            display.textContent += btn.id;
+        };
+        displayValue = display.textContent;
+    });
+    
+})
